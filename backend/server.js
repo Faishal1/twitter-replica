36008080863980e7450aee8aca5857591f96c08e
Faishal <tahseen@blickx.com>
2018-10-29 15:19:29 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var mongoose = require('./mongoose'),
+  passport = require('passport'),
+  express = require('express'),
+  jwt = require('jsonwebtoken'),
+  expressJwt = require('express-jwt'),
+  router = express.Router(),
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  twitterConfig = require('./twitter.config.js');
+
+mongoose();
+
+var User = require('mongoose').model('User');
+var passportConfig = require('./passport');
+
+
+
+var app = express();
+
+// enable cors
+var corsOption = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token']
+};
+app.use(cors(corsOption));
+
+//rest API requirements
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+router.route('/auth/twitter/reverse')
+  .post(function(req, res) {
+    request.post({
+      url: 'https://api.twitter.com/oauth/request_token',
+      oauth: {
+        oauth_callback: "http%3A%2F%2Ftest.lpgsuvidha.com",
+        consumer_key: twitterConfig.consumerKey,
+        consumer_secret: twitterConfig.consumerSecret
+      }
+    }, function (err, r, body) {
+      if (err) {
+        return res.send(500, { message: e.message });
+      }
+
+      var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
+      res.send(JSON.parse(jsonStr));
+    });
+  });
+
+
+
+app.use('/api/v1', router);
+
+app.listen(4000);
+module.exports = app;
+
+console.log('Server running at http://localhost:4000/');
---
+++ backend/twitter.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumerKey: '6TbQGhjI2rDmRxlSuFX4Su1UF',
+  consumerSecret: '9ZDtNb80w6fDPVx96tlefwsr2VXPjHYoV9bCTbHtvkgmlBLSQ7',
+  access_token_key: '1056239200398270465-OuAmVgZwkMUW8oRs1TwXQ7dKPFVW0h',
+  access_token_secret: 'KKnDMubIih8qchub7kQXFaUl5lLnl94rIx1TwJi1yGMIN'
+
+};
